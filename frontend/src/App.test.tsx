88f0be5e4e000f2abc41ba55f609e8a43df3b6c9
Austin Nance <austin.james.nance@gmail.com>
2026-08-13 23:33:01 -0500

    restoring file: frontend/src/App.test.tsx

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,8 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-const heading = screen.getByText(/THIS SHOULD FAIL/i);
-
 test("renders cloud infrastructure heading", () => {
   render(<App />);
 
